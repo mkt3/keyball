ec2fc98722b0e743dc467dc9337d6e5bdb618cd4
Makoto Morinaga <makoto@mkt3.dev>
2024-11-25 01:34:31 +0900

    Add custom keycodes for special characters

--- qmk_firmware/keyboards/keyball/keyball44/keymaps/mkt3_keymap/keymap.c
+++ qmk_firmware/keyboards/keyball/keyball44/keymaps/mkt3_keymap/keymap.c
@@ -22,7 +22,6 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 enum layer_number {
   _BASE = 0,
-  _CTRL,
   _SYMBOL,
   _TILE,
   _MOUSE,
@@ -35,17 +34,16 @@ enum layer_number {
 #define HSCL_DOT LT(_HSCL_MNG,KC_DOT)
 
 #define SYM_SPC LT(_SYMBOL,KC_SPC)
-#define TILE_MINS LT(_TILE,KC_MINS)
+#define TILE_GRV LT(_TILE,KC_GRV)
 
-#define LCTL_MINS LT(_CTRL,KC_MINS)
-#define RCTL_QUOT LT(_CTRL,KC_QUOT)
+#define LCTL_QUOT LCTL_T(KC_QUOT)
 
 #define LSFT_GRV LSFT_T(KC_GRV)
 #define RSFT_BSLS RSFT_T(KC_BSLS)
+#define RSFT_EQL RSFT_T(KC_EQL)
 
-#define LGUI_SPC LGUI_T(KC_SPC)
 #define LGUI_UNDS LGUI_T(KC_UNDS)
-#define RGUI_EQL RGUI_T(KC_EQL)
+
 
 enum {
     TD_QESC,
@@ -59,44 +57,37 @@ tap_dance_action_t tap_dance_actions[] = {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_universal(
     KC_TAB   ,TD(TD_QESC), KC_W    , KC_E     , KC_R     , KC_T     ,                                        KC_Y     , KC_U     , KC_I     , KC_O     , KC_P     , KC_LBRC  ,
-    MO(_CTRL), KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , RCTL_QUOT  ,
+    KC_LCTL, KC_A     , KC_S     , KC_D     , KC_F     , KC_G     ,                                        KC_H     , KC_J     , KC_K     , KC_L     , KC_SCLN  , LCTL_QUOT  ,
     LSFT_GRV , KC_Z     , KC_X     , KC_C     , KC_V     , KC_B     ,                                        KC_N     , KC_M     , VSCL_COMM, HSCL_DOT , KC_SLSH  , RSFT_BSLS,
-               KC_LALT  ,KC_GRV    , TILE_MINS,LGUI_SPC,KC_LSFT,                               RGUI_EQL,SYM_SPC       , KC_NO,KC_NO                    , KC_ESC
-  ),
-
-  [_CTRL] = LAYOUT_universal(
-    C(KC_TAB), C(KC_Q)  , C(KC_W)  , C(KC_E)  , C(KC_R)  , C(KC_T)  ,                                        C(KC_Y)  , C(KC_U)  , KC_TAB  , C(KC_O)  , KC_UP    , C(KC_LBRC)  ,
-    MO(_CTRL), C(KC_A)  , C(KC_S)  , KC_DEL   , KC_RGHT  , C(KC_G)  ,                                        KC_BSPC  , C(KC_J)  , C(KC_K)  , C(KC_L)  , C(KC_SCLN), C(KC_QUOT) ,
-    KC_LSFT ,  C(KC_Z)  , C(KC_X)  , C(KC_C)  , C(KC_V)  , KC_LEFT  ,                                        KC_DOWN  , KC_ENT   ,C(KC_COMM), C(KC_DOT), C(KC_SLSH), KC_RSFT,
-               _______ ,_______   , C(KC_MINS),_______,_______,                                  C(KC_EQL),C(KC_SPC)  , _______,_______                    , _______
+               KC_LALT  ,KC_GRV    , TILE_GRV,LGUI_UNDS,KC_LSFT,                               RSFT_EQL,SYM_SPC       , KC_NO,KC_NO                    , KC_ESC
   ),
 
   [_SYMBOL] = LAYOUT_universal(
-    _______   , S(KC_1) , S(KC_2)  , S(KC_3)  , S(KC_4)  , S( KC_5) ,                                        S(KC_6)  , S(KC_7)  , S(KC_8)  ,S(KC_9)   , S(KC_0)     , KC_MINS  ,
+    S(KC_GRV) , S(KC_1) , S(KC_2)  , S(KC_3)  , S(KC_4)  , S( KC_5) ,                                        S(KC_6)  , S(KC_7)  , S(KC_8)  ,S(KC_9)   , S(KC_0)     , KC_MINS  ,
     KC_LCTL , KC_1     , KC_2     , KC_3     , KC_4     , KC_5     ,                                         KC_6     , KC_7     , KC_8     , KC_9     , KC_0     , KC_EQL  ,
-    S(KC_GRV),KC_SLSH  , KC_LBRC  , KC_RBRC  , S(KC_LBRC),S(KC_RBRC),                                        KC_EQL   , S(KC_EQL),S(KC_COMM), S(KC_DOT) , S(KC_SLSH), S(KC_BSLS),
-               _______ ,_______   , S(KC_MINS),_______,_______,                                  _______,_______       , _______,_______                    , _______
+    KC_LSFT ,KC_SLSH  , KC_LBRC  , KC_RBRC  , S(KC_LBRC),S(KC_RBRC),                                        KC_EQL   , S(KC_EQL),S(KC_COMM), S(KC_DOT) , S(KC_SLSH), KC_RSFT,
+               _______ ,_______   , S(KC_GRV),S(KC_MINS),_______,                              S(KC_EQL),_______       , _______,_______                    , _______
   ),
 
   [_TILE] = LAYOUT_universal(
     KC_NO ,  G(KC_Q)   , KC_NO    , KC_NO   , KC_NO    , KC_NO    ,                                        KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO   , KC_NO   ,
     A(KC_6) , A(KC_1)  , A(KC_2)  , A(KC_3) , A(KC_4)  , A(KC_5)   ,                                      A(KC_H)  , A(KC_J)  , A(KC_K)  , A(KC_L)  , A(KC_6) , KC_NO   ,
     KC_LSFT , KC_NO    , KC_NO    , G(KC_C) , A(KC_V)  , KC_NO  ,                                                KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO   , KC_RSFT ,
-              KC_NO  , KC_NO     , KC_NO  , KC_NO  , KC_LSFT  ,                                     KC_NO  , A(KC_SPC)  , KC_NO       , KC_NO  , KC_NO
+              KC_NO  , KC_NO     , KC_NO  , KC_NO  , KC_LSFT  ,                                KC_NO  , A(KC_SPC)  , KC_NO       , KC_NO  , KC_NO
   ),
 
   [_MOUSE] = LAYOUT_universal(
-    KC_NO ,  KC_NO   , KC_NO    , KC_NO   , KC_NO    , KC_NO    ,                                         KC_NO    , KC_NO  , KC_NO    , KC_NO    , KC_NO   , KC_NO   ,
-    KC_LCTL ,  KC_NO   , KC_NO    , KC_NO   , KC_NO    , KC_NO    ,                                       KC_NO    , KC_J   , KC_K    , KC_BTN1   , KC_BTN2   , KC_NO   ,
-    KC_LSFT ,  KC_NO , KC_NO  , KC_C , KC_V  , KC_NO  ,                                                   KC_NO  , KC_NO    , KC_NO  , KC_NO      , KC_NO  , KC_NO  ,
-               KC_NO  , KC_NO       , KC_NO , KC_LGUI ,KC_LSFT,                                    KC_NO , KC_N             , KC_NO  , KC_NO      , KC_NO
+    KC_NO ,  KC_NO   , KC_NO    , KC_NO   , KC_NO    , KC_NO    ,                                         KC_NO    , KC_NO  , KC_NO  , KC_NO    , KC_NO   , KC_NO   ,
+    KC_LCTL ,  KC_NO   , KC_NO    , KC_NO   , KC_NO    , KC_NO    ,                                       KC_NO    , KC_J   , KC_K   , KC_BTN1  , KC_BTN2 , KC_NO   ,
+    KC_LSFT ,  KC_NO , KC_NO  , KC_C , KC_V  , KC_NO  ,                                                   KC_NO  , KC_NO    , KC_NO  , KC_NO    , KC_NO   , KC_NO  ,
+               KC_NO  , KC_NO       , KC_NO , KC_LGUI ,KC_LSFT,                                KC_NO , KC_NO            , KC_NO  , KC_NO    , KC_NO
   ),
 
   [_VSCL_FN] = LAYOUT_universal(
-    KC_NO ,  KC_BRID  , KC_BRIU , KC_NO   , KC_NO    , KC_NO    ,                                         KC_MPRV    , KC_MPLY    , KC_MNXT    , KC_MUTE    , KC_VOLD   , KC_VOLU   ,
-    KC_NO ,  KC_F1 ,  KC_F2   , KC_F3    , KC_F4   , KC_F5    ,                                           KC_F6    , KC_F7    , KC_F8    , KC_F9    , KC_F10   , KC_NO   ,
-    KC_LSFT ,KC_NO , KC_NO  , KC_NO , KC_NO  , KC_F11  ,                                                  KC_F12  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_RSFT  ,
-             KC_NO  , KC_NO , KC_NO  ,         KC_NO  , KC_NO  ,                                 KC_NO  , KC_NO  , KC_NO       , KC_NO  , KC_NO
+    KC_MPRV , KC_MPLY , KC_MNXT , KC_MUTE , KC_VOLD , KC_VOLU ,                                           KC_NO  ,  KC_NO    , KC_NO   , KC_NO   , KC_NO   , KC_NO   ,
+    KC_NO   , KC_F1   , KC_F2   , KC_F3   , KC_F4   , KC_F5   ,                                           KC_F6  , KC_F7   , KC_F8   , KC_F9   , KC_F10  , KC_NO   ,
+    KC_LSFT , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_F11  ,                                           KC_F12 , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_RSFT ,
+             KC_BRID  , KC_BRIU , KC_NO  ,         KC_NO  , KC_NO  ,                           KC_NO  , KC_NO            , KC_NO  , KC_NO        , KC_NO
   ),
 
   [_HSCL_MNG] = LAYOUT_universal(
@@ -142,21 +133,17 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
         case SYM_SPC:
             return true;
-        case TILE_MINS:
+        case TILE_GRV:
             return true;
-        case LCTL_MINS:
-            return true;
-        case RCTL_QUOT:
+        case LCTL_QUOT:
             return true;
         case LSFT_GRV:
             return true;
         case RSFT_BSLS:
             return true;
-        case LGUI_SPC:
+        case RSFT_EQL:
             return true;
         case LGUI_UNDS:
-            return true;
-        case RGUI_EQL:
             return true;
         default:
             // Do not select the hold action when another key is pressed.
@@ -164,46 +151,66 @@ bool get_hold_on_other_key_press(uint16_t keycode, keyrecord_t *record) {
     }
 }
 
-/* bool process_control_key(uint16_t replacement, keyrecord_t *record) { */
-/*     static uint8_t active_ctrl = 0; */
-/*     static bool replacement_active = false; */
-/*     uint8_t mod_state = get_mods(); */
 
-/*     if (record->event.pressed) { */
-/*         if ((mod_state & MOD_MASK_CTRL) && !(mod_state & ~(MOD_MASK_CTRL))) { */
-/*             active_ctrl = mod_state & MOD_MASK_CTRL; */
-/*             del_mods(MOD_MASK_CTRL); */
-/*             send_keyboard_report(); */
-/*             register_code(replacement); */
-/*             replacement_active = true; */
-/*             return false; */
-/*         } */
-/*     } else { */
-/*         if (replacement_active) { */
-/*             unregister_code(replacement); */
-/*             replacement_active = false; */
+static bool left_ctrl_held = false;
 
-/*             if (active_ctrl) { */
-/*                 set_mods(active_ctrl); */
-/*                 send_keyboard_report(); */
-/*                 active_ctrl = 0; */
-/*             } */
-/*             return false; */
-/*         } */
-/*     } */
-/*     return true; */
-/* } */
+bool process_control_key(uint16_t replacement, keyrecord_t *record) {
+    static bool replacement_active = false;
 
-/* bool process_record_user(uint16_t keycode, keyrecord_t *record) { */
-/*     switch (keycode) { */
-/*         case KC_H: */
-/*             return process_control_key(KC_BSPC, record); */
-/*         case KC_D: */
-/*             return process_control_key(KC_DELETE, record); */
-/*         case KC_M: */
-/*             return process_control_key(KC_ENTER, record); */
-/*         case KC_I: */
-/*             return process_control_key(KC_TAB, record); */
-/*     } */
-/*     return true; */
-/* } */
+    if (record->event.pressed) {
+        if (left_ctrl_held && !replacement_active) {
+            replacement_active = true;
+            del_mods(MOD_MASK_CTRL);
+            send_keyboard_report();
+            register_code(replacement);
+            return false;
+        }
+    } else {
+        if (replacement_active) {
+            unregister_code(replacement);
+            replacement_active = false;
+        }
+
+        if (left_ctrl_held) {
+            register_code(KC_LCTL);
+            send_keyboard_report();
+        }
+    }
+
+    return true;
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    if (record->event.pressed) {
+        if (keycode == KC_LCTL) {
+            left_ctrl_held = true;
+            return true;
+        }
+    } else {
+        if (keycode == KC_LCTL) {
+            left_ctrl_held = false;
+            return true;
+        }
+    }
+
+    switch (keycode) {
+        case KC_H:
+            return process_control_key(KC_BSPC, record);
+        case KC_D:
+            return process_control_key(KC_DELETE, record);
+        case KC_M:
+            return process_control_key(KC_ENTER, record);
+        case KC_I:
+            return process_control_key(KC_TAB, record);
+        case KC_B:
+            return process_control_key(KC_LEFT, record);
+        case KC_F:
+            return process_control_key(KC_RIGHT, record);
+        case KC_P:
+            return process_control_key(KC_UP, record);
+        case KC_N:
+            return process_control_key(KC_DOWN, record);
+    }
+
+    return true;
+}
